e79de28f8785e573c7f8d9293187045f5841fc28
tyler <tylerhaddams@gmail.com>
2023-09-19 21:04:02 -0600

    more skeleton code

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
-# from typing import (functions)
+from typing import Callable
 import sys
 
-class FinanceData(object):
+class FinanceState(object):
     def __init__(self, cash = 0):
-        self.cash = [cash]
+        self.cash = cash
+
+
+class FinanceData(object):
+    def __init__(self, event: FinanceState):
+        self.data: list[FinanceState] = []
+        if not FinanceState is None:
+            self.data.append(event)
     
-    def appendEvent(self, cash = 0, **kwargs):
-        self.cash.append(cash)
+    def appendEvent(self, event: FinanceState):
+        self.data.append(event)
 
 # todo: create a list of functions (events) that will be called (happen) once every time period according to the granularity.
 # an "event" will possibly append a state to the FinanceData object (more/less cash, etc.)
-financeEvents = []
+financeEvents: list[Callable[[FinanceState, int, float], FinanceState]] = []
 
 if __name__ == '__main__':
     if len(sys.argv) <= 2:
@@ -29,4 +36,11 @@ if __name__ == '__main__':
     if not timePeriod.isnumeric():
         print("time-period must be numeric, got {}".format(timePeriod))
         exit(1)
-    pass
+
+    financeData = FinanceData(0)
+
+    for eventIdx in range(int(granularity) * int(timePeriod)):
+        newState = financeData.data[-1]
+        for funct in financeEvents:
+            newState = funct(financeData.data[-1], eventIdx, float(granularity))
+            financeData.appendEvent(newState)
