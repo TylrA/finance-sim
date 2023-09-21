3330830c9ec57f00439fa90136059e01e84614f5
tyler <tylerhaddams@gmail.com>
2023-09-20 18:21:55 -0600

    add constant salaried income and constant expense

--- src/main.py
+++ src/main.py
@@ -2,11 +2,15 @@ from typing import Callable
 import sys
 
 class FinanceState(object):
-    def __init__(self, cash = 0):
+    def __init__(self, cash: float = 0):
         self.cash = cash
 
+    def copy(self):
+        result = FinanceState(self.cash)
+        return result
 
-class FinanceData(object):
+
+class FinanceHistory(object):
     def __init__(self, event: FinanceState):
         self.data: list[FinanceState] = []
         if not FinanceState is None:
@@ -16,8 +20,23 @@ class FinanceData(object):
         self.data.append(event)
 
 # todo: create a list of functions (events) that will be called (happen) once every time period according to the granularity.
-# an "event" will possibly append a state to the FinanceData object (more/less cash, etc.)
-financeEvents: list[Callable[[FinanceState, int, float], FinanceState]] = []
+# an "event" will possibly append a state to the FinanceHistory object (more/less cash, etc.)
+FinanceEvent = Callable[[FinanceState, int, float], FinanceState]
+financeEvents: list[FinanceEvent] = []
+
+def constantSalariedIncome(salary: float) -> FinanceEvent:
+    def incomeEvent(state: FinanceState, period: int, yearFraction: float):
+        result = state.copy()
+        result.cash += salary * yearFraction
+        return result
+    return incomeEvent
+
+def constantExpense(yearlyExpense: float) -> FinanceEvent:
+    def expenseEvent(state: FinanceState, period: int, yearFraction: float):
+        result = state.copy()
+        result.cash -= yearlyExpense * yearFraction
+        return result
+    return expenseEvent
 
 if __name__ == '__main__':
     if len(sys.argv) <= 2:
@@ -37,7 +56,7 @@ if __name__ == '__main__':
         print("time-period must be numeric, got {}".format(timePeriod))
         exit(1)
 
-    financeData = FinanceData(0)
+    financeData = FinanceHistory(FinanceState(0))
 
     for eventIdx in range(int(granularity) * int(timePeriod)):
         newState = financeData.data[-1]
