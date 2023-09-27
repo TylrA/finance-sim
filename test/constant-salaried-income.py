2b9782ae410c0d416b77628f86513026b025a3e3
tyler <tylerhaddams@gmail.com>
2023-09-26 19:08:25 -0600

    add test for constant expense

--- test/constant-salaried-income.py
+++ test/constant-salaried-income.py
@@ -23,3 +23,23 @@ def testSalariedIncomeBiMonthly():
         assert newCash == pytest.approx(previousCash + 5)
         previousCash = newCash
     
+def testSalariedIncomeBiAnnual():
+    financeData = FinanceHistory(FinanceState(0))
+    events: list[FinanceEvent] = [constantSalariedIncome(100)]
+    financeData.setEventComponents(events)
+    previousCash = financeData.data[0].cash
+    for idx in range(1, 10):
+        financeData.passEvent(idx, 2)
+        newCash = financeData.data[idx].cash
+        assert newCash == pytest.approx(previousCash + 200)
+        previousCash = newCash
+
+def testSalariedIncomeZero():
+    financeData = FinanceHistory(FinanceState(0))
+    events: list[FinanceEvent] = [constantSalariedIncome(0)]
+    financeData.setEventComponents(events)
+    for idx in range(1, 10):
+        financeData.passEvent(idx, 2)
+        newCash = financeData.data[idx].cash
+        assert newCash == 0
+
