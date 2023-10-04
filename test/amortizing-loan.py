dfee06e771c14928e7cd94f2a372a5b757828892
tyler <tylerhaddams@gmail.com>
2023-10-03 21:49:57 -0600

    more tests for amortizing loans

--- test/amortizing-loan.py
+++ test/amortizing-loan.py
@@ -2,7 +2,7 @@ import pytest
 from finance_sim import *
 
 def testAmortizingLoanTerm():
-    loan = AmortizingLoan('test', 0, 100000)
+    loan = AmortizingLoan('test', 0, 100000, 0.05, 20)
     state = FinanceState(100000)
     state.amortizingLoans['test'] = loan
     state = makeAmortizedPayments(state, 0, 1 / 12)
@@ -11,3 +11,32 @@ def testAmortizingLoanTerm():
         state = makeAmortizedPayments(state, i, 1 / 12)
     assert state.amortizingLoans['test'].term == pytest.approx(19)
 
+def testAmortizingLoanFullPayment():
+    loan = AmortizingLoan('test', 0, 100000, 0.05, 20)
+    state = FinanceState(600000)
+    state.amortizingLoans['test'] = loan
+    assert state.amortizingLoans['test'].principle == 0
+    for i in range(12 * 20):
+        state = makeAmortizedPayments(state, i, 1 / 12)
+    assert state.amortizingLoans['test'].principle == pytest.approx(0)
+
+def testAmortizingLoanConstantPaymentCost():
+    loan = AmortizingLoan('test', 0, 100000, 0.05, 20)
+    state = FinanceState(600000)
+    state.amortizingLoans['test'] = loan
+    state = makeAmortizedPayments(state, 0, 1 / 12)
+    previousCash = state.cash
+    paymentAmount = 600000 - previousCash
+    for i in range(10):
+        state = makeAmortizedPayments(state, i, 1 / 12)
+        assert previousCash - state.cash == pytest.approx(paymentAmount)
+        previousCash = state.cash
+
+def testAmortizingLoanInterestRate():
+    loan = AmortizingLoan('test', 0, 100000, 0.05, 20)
+    state = FinanceState(600000)
+    state.amortizingLoans['test'] = loan
+    state = makeAmortizedPayments(state, 0, 1)
+    paymentAmount = 600000 - state.cash
+    interestPayment = paymentAmount - state.amortizingLoans['test'].principle
+    assert interestPayment == pytest.approx(600000 * 0.05)
