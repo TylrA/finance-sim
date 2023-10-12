d6b946b28b1cf2e267ecbea43f2ec63bf834e5d2
tyler <tylerhaddams@gmail.com>
2023-10-11 18:39:52 -0600

    add some configuration types

--- src/finance_sim/config.py
+++ src/finance_sim/config.py
@@ -1,4 +1,32 @@
 import yaml
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class TimeConfig(object):
+    granularity: str
+    period: int
+
+class StateType:
+    cash = 1
+    constantGrowthAsset = 2
+
+@dataclass
+class StateConfig(object):
+    type: StateType
+    value: float
+    name: str
+
+@dataclass
+class ScheduledState(object):
+    state: StateConfig
+    schedule: datetime
+
+@dataclass
+class ScenarioConfig(object):
+    time: TimeConfig
+    initialValues: list[StateConfig]
+    scheduledValues: list[ScheduledState]
 
 def parseConfig(path: str):
     with open(path, 'r') as configFile:
