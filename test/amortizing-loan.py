e84378c621b653d1c19a1302c778c41b089e914b
tyler <tylerhaddams@gmail.com>
2023-10-16 23:23:11 -0600

    update amortizing loan unit tests to use date

--- test/amortizing-loan.py
+++ test/amortizing-loan.py
@@ -1,46 +1,56 @@
 import pytest
 from finance_sim import *
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 def testAmortizingLoanTerm():
     loan = AmortizingLoan('test', 0, 100000, 0.05, 20)
-    state = FinanceState(100000)
+    state = FinanceState(date(1999, 12, 1))
+    state.cash = 100000
     history = FinanceHistory(state)
     state.amortizingLoans['test'] = loan
-    state = makeAmortizedPayments(history, state, 0, 1 / 12)
+    delta = relativedelta(months=1)
+    state = makeAmortizedPayments(history, state, date(2000, 1, 1), delta)
     assert state.amortizingLoans['test'].term == pytest.approx(20 - 1 / 12)
-    for i in range(1, 12):
-        state = makeAmortizedPayments(history, state, i, 1 / 12)
+    for month in range(2, 13):
+        state = makeAmortizedPayments(history, state, date(2000, month, 1), delta)
     assert state.amortizingLoans['test'].term == pytest.approx(19)
 
 def testAmortizingLoanFullPayment():
     loan = AmortizingLoan('test', 0, 100000, 0.05, 20)
-    state = FinanceState(600000)
+    state = FinanceState(date(1999, 12, 1))
+    state.cash = 600000
     history = FinanceHistory(state)
     state.amortizingLoans['test'] = loan
     assert state.amortizingLoans['test'].principle == 0
-    for i in range(12 * 20):
-        state = makeAmortizedPayments(history, state, i, 1 / 12)
+    delta = relativedelta(months=1)
+    for year in range(2000,2020):
+        for month in range(1, 13):
+            state = makeAmortizedPayments(history, state, date(year, month, 1), delta)
     assert state.amortizingLoans['test'].principle == pytest.approx(100000)
 
 def testAmortizingLoanConstantPaymentCost():
     loan = AmortizingLoan('test', 0, 100000, 0.05, 20)
-    state = FinanceState(600000)
+    state = FinanceState(date(1999, 12, 1))
+    state.cash = 600000
     history = FinanceHistory(state)
     state.amortizingLoans['test'] = loan
-    state = makeAmortizedPayments(history, state, 0, 1 / 12)
+    delta = relativedelta(months=1)
+    state = makeAmortizedPayments(history, state, date(2000, 1, 1), delta)
     previousCash = state.cash
     paymentAmount = 600000 - previousCash
-    for i in range(10):
-        state = makeAmortizedPayments(history, state, i, 1 / 12)
+    for month in range(2, 13):
+        state = makeAmortizedPayments(history, state, date(2000, month, 1), delta)
         assert previousCash - state.cash == pytest.approx(paymentAmount, 1e-3)
         previousCash = state.cash
 
 def testAmortizingLoanInterestRate():
     loan = AmortizingLoan('test', 0, 100000, 0.05, 20)
-    state = FinanceState(600000)
+    state = FinanceState(date(2000, 1, 1))
+    state.cash = 600000
     history = FinanceHistory(state)
     state.amortizingLoans['test'] = loan
-    state = makeAmortizedPayments(history, state, 0, 1)
+    state = makeAmortizedPayments(history, state, date(2001, 1, 1), relativedelta(years=1))
     paymentAmount = 600000 - state.cash
     interestPayment = paymentAmount - state.amortizingLoans['test'].principle
     assert interestPayment == pytest.approx(100000 * 0.05, 1e-6)
