a3c604818bad46e2bb4a3a48da8b7f04d3d8b6a2
tyler <tylerhaddams@gmail.com>
2023-10-24 23:50:35 -0600

    fix tests for constant salaried income

--- test/constant-salaried-income.py
+++ test/constant-salaried-income.py
@@ -1,5 +1,6 @@
 import pytest
 from finance_sim import *
+from calendar import monthrange
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
@@ -11,42 +12,52 @@ def testSalariedIncomeMonthly():
     delta = relativedelta(months=1)
     for month in range(2, 100):
         financeData.passEvent(date(2000 + (month) // 12, ((month - 1) % 12) + 1, 1), delta)
-        newCash = financeData.data[idx].cash
+        newCash = financeData.latestEvent().cash
         assert newCash == pytest.approx(previousCash + 10)
         previousCash = newCash
     
 def testSalariedIncomeSemiMonthly():
-    financeData = FinanceHistory(FinanceState(date(2000, 1, 1)))
-    events: list[FinanceEvent] = [constantSalariedIncome(120, AccrualModel.ProRata)]
+    financeData = FinanceHistory(FinanceState(date(2000, 1, 15)))
+    events: list[FinanceEvent] = [constantSalariedIncome(120, AccrualModel.PeriodicSemiMonthly)]
     financeData.setEventComponents(events)
     previousCash = financeData.data[0].cash
     delta = relativedelta(months=1)
     for idx in range(1, 100):
-        month = idx // 2 + 1
-        day = 15 if month % 2 else 1
-        adjustedDate = date(2000 + (month // 12), month, day)
-        if month % 2:
-            delta = relativedelta(days=14)
+        month = (idx // 2) % 12 + 1
+        year = 2000 + (month // 12)
+        day = monthrange(year, month)[1] if idx % 2 else 15
+        adjustedDate = date(year, month, day)
+        if idx % 2:
+            delta = relativedelta(days = day - 15)
         else:
-            delta = relativedelta(days=(int(((date(2000 + (month // 12), month, 1) \
-                                              + relativedelta(months=1)) - adjustedDate)
-                                            .total_seconds()) // (24 * 3600)))
+            delta = relativedelta(days = 15)
         financeData.passEvent(adjustedDate, delta)
-        newCash = financeData.data[idx].cash
+        newCash = financeData.latestEvent().cash
         assert newCash == pytest.approx(previousCash + 5)
         previousCash = newCash
     
-def testSalariedIncomeBiAnnual():
+def testSalariedIncomeSemiAnnual():
     financeData = FinanceHistory(FinanceState(date(1999, 6, 1)))
-    events: list[FinanceEvent] = [constantSalariedIncome(100, AccrualModel.ProRata)]
+    events: list[FinanceEvent] = [constantSalariedIncome(100, AccrualModel.PeriodicMonthly)]
     financeData.setEventComponents(events)
     previousCash = financeData.data[0].cash
     for year in range(2000, 2006):
         for month in [1, 6]:
             financeData.passEvent(date(year, month, 1), relativedelta(months=6))
-            newCash = financeData.data[idx].cash
-            assert newCash == pytest.approx(previousCash + 200)
+            newCash = financeData.latestEvent().cash
+            assert newCash == pytest.approx(previousCash + 50)
             previousCash = newCash
+
+def testSalariedIncomeBiAnnual():
+    financeData = FinanceHistory(FinanceState(date(1999, 1, 1)))
+    events: list[FinanceEvent] = [constantSalariedIncome(100, AccrualModel.PeriodicYearly)]
+    financeData.setEventComponents(events)
+    previousCash = financeData.data[0].cash
+    for year in range(2000, 2009, 2):
+        financeData.passEvent(date(year, 1, 1), relativedelta(years=2))
+        newCash = financeData.latestEvent().cash
+        assert newCash == pytest.approx(previousCash + 200)
+        previousCash = newCash
 
 def testSalariedIncomeZero():
     financeData = FinanceHistory(FinanceState(date(1999, 12, 1)))
@@ -54,6 +65,6 @@ def testSalariedIncomeZero():
     financeData.setEventComponents(events)
     for month in range(1, 13):
         financeData.passEvent(date(2000, month, 1), relativedelta(months=1))
-        newCash = financeData.data[idx].cash
+        newCash = financeData.latestEvent().cash
         assert newCash == 0
 
