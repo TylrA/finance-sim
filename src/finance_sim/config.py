5376379e7eab0a1e9e7a048857095e2839ddd805
tyler <tylerhaddams@gmail.com>
2023-10-31 21:01:21 -0600

    add parseGranularity and parseAccrualModel

--- src/finance_sim/config.py
+++ src/finance_sim/config.py
@@ -1,11 +1,17 @@
+from ctypes import ArgumentError
+import re
 import yaml
 from dataclasses import dataclass
 from datetime import date
+from dateutil.relativedelta import relativedelta
 from enum import Enum
+
+from finance_sim.scheduling import AccrualModel
 
 @dataclass
 class TimeConfig(object):
-    granularity: date
+    granularity: relativedelta
+    accrualModel: AccrualModel
     period: int
 
 class StateType(Enum):
@@ -29,6 +35,52 @@ class ScenarioConfig(object):
     initialValues: list[StateConfig]
     scheduledValues: list[ScheduledState]
 
+def parseGranularity(granularityStr: str) -> relativedelta:
+    pattern = r'(\d+)\s*(d|w|M2?|Y)'
+    match = re.match(pattern, granularityStr)
+    if not match:
+        raise ArgumentError('granularityStr must match the regex pattern {}, got {}'
+                            .format(pattern, granularityStr))
+
+    groups = match.groups()
+    value = int(groups[1])
+    unit = groups[2]
+    if unit == 'd':
+        return relativedelta(days = value)
+    if unit == 'w':
+        return relativedelta(days = 7 * value)
+    if unit == 'M':
+        return relativedelta(months = value)
+    if unit == 'M2':
+        return relativedelta(days = 15 * value)
+    if unit == 'Y':
+        return relativedelta(years = value)
+
+    raise RuntimeError('None of the supported units was used')
+
+def parseAccrualModel(accrualModelStr: str) -> AccrualModel:
+    pattern = r'(pro rata|periodic (?:monthly|semi ?monthly|weekly|biweekly|yearly))'
+    match = re.match(pattern, accrualModelStr)
+    if not match:
+        raise ArgumentError('accrualModelStr must match the regex pattern {}, got {}'
+                            .format(pattern, accrualModelStr))
+
+    if accrualModelStr == 'pro rata':
+        return AccrualModel.ProRata
+    if accrualModelStr == 'periodic monthly':
+        return AccrualModel.PeriodicMonthly
+    if accrualModelStr == 'periodic semi monthly' or \
+       accrualModelStr == 'periodic semimonthly':
+        return AccrualModel.PeriodicSemiMonthly
+    if accrualModelStr == 'periodic weekly':
+        return AccrualModel.PeriodicWeekly
+    if accrualModelStr == 'periodic biweekly':
+        return AccrualModel.PeriodicBiweekly
+    if accrualModelStr == 'periodic yearly':
+        return AccrualModel.PeriodicYearly
+
+    raise RuntimeError('None of the supported accrual model was used')
+
 def parseConfig(path: str) -> ScenarioConfig:
     with open(path, 'r') as configFile:
         rawConfig = yaml.parse(configFile)
@@ -37,8 +89,9 @@ def parseConfig(path: str) -> ScenarioConfig:
         if 'initialState' not in rawConfig:
             raise RuntimeError('Configuration requires an "initialState" field')
         rawTimeConfig = rawConfig['time']
-        if 'granularity' not in rawTimeConfig or 'period' not in rawTimeConfig:
-            raise RuntimeError('"time" field requires "granularity" and "period"')
+        if 'period' not in rawTimeConfig:
+            raise RuntimeError('"time" field requires "period"')
         timeConfig = TimeConfig(
-            granularity=date.fromisoformat(rawTimeConfig['granularity']),
+            granularity=.fromisoformat(rawTimeConfig['granularity']),
             period=int(rawTimeConfig['period']))
+        
