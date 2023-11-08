e78deb6a4a7e69dbfb2f28ec7b0787a40be1133d
tyler <tylerhaddams@gmail.com>
2023-11-07 21:07:04 -0700

    update tests

--- test/config.py
+++ test/config.py
@@ -17,10 +17,10 @@ def testParseConfigInitialState():
     assert len(config.initialState) == 2
     assert config.initialState[0].type == StateType.cash
     assert config.initialState[0].name == "Savings"
-    assert config.initialState[0].value == 100
+    assert config.initialState[0].data == {"value": 100}
     assert config.initialState[1].type == StateType.constantGrowthAsset
     assert config.initialState[1].name == "CD Savings"
-    assert config.initialState[1].value == 500
+    assert config.initialState[1].data == {"value": 500, "appreciation": 0.05}
     # todo: add appreciation
 
 def testParseConfigScheduledUpdates():
@@ -31,4 +31,4 @@ def testParseConfigScheduledUpdates():
     assert config.scheduledValues[0].endDate == date(2040, 4, 30)
     assert config.scheduledValues[0].state.name == "inheritance"
     assert config.scheduledValues[0].state.type == StateType.cash
-    assert config.scheduledValues[0].state.value == 5000
+    assert config.scheduledValues[0].state.data == {"value": 5000}
