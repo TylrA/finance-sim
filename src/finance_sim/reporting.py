5267e7d68f929957f48fe217f45387566e44dd4c
tyler <tylerhaddams@gmail.com>
2023-11-10 22:14:56 -0700

    create initial finance state from config object

--- src/finance_sim/reporting.py
+++ src/finance_sim/reporting.py
@@ -1,5 +1,24 @@
-from finance_sim.config import ScenarioConfig
+from finance_sim.config import ScenarioConfig, StateType
+import finance_sim.main as finSim
+
+from datetime import date
 from pandas import DataFrame
 
+def assembleInitialState(config: ScenarioConfig) \
+    -> finSim.FinanceState:
+    result = finSim.FinanceState(config.time.startingDate)
+    for stateConfig in config.initialState:
+        if stateConfig.type == StateType.cash:
+            result.cash += float(stateConfig.data['value'])
+        elif stateConfig.type == StateType.constantGrowthAsset:
+            result.constantGrowthAssets.append(
+                finSim.ConstantGrowthAsset(
+                    accrualModel=config.time.accrualModel,
+                    initialValue=stateConfig.data['value'],
+                    annualAppreciation=stateConfig.data['appreciation']))
+    return result
+
 def report(config: ScenarioConfig) -> DataFrame:
+    initialState = assembleInitialState(config)
+    history = finSim.FinanceHistory(initialState)
     return DataFrame()
