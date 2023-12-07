4f86d7344d0eefadf32a6c1db7ade2054dec8be6
tyler <tylerhaddams@gmail.com>
2023-12-07 00:26:46 -0700

    fix constant growth asset tests

--- test/constant-growth-asset.py
+++ test/constant-growth-asset.py
@@ -4,28 +4,32 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 def testConstantGrowthMonthly():
-    initialState = FinanceState(date(1999, 12, 1))
-    initialState.constantGrowthAssets.append(
-        ConstantGrowthAsset(AccrualModel.PeriodicMonthly, 100, 0.5))
-    financeData = FinanceHistory(initialState)
-    financeData.setEventComponents([appreciateConstantAssets])
+    eventGroup = EventGroup(date(1999, 12, 1),
+                            { 'g': ConstantGrowthAsset('g',
+                                                       AccrualModel.PeriodicMonthly,
+                                                       100,
+                                                       0.5),
+                              'cash': CashEvent('cash', 0) })
+    financeData = FinanceHistory(eventGroup)
     monthlyInterest = 1.5 ** (1 / 12)
     delta = relativedelta(months=1)
     for month in range(1, 10):
         financeData.passEvent(date(2000, month, 1), delta)
-        assert financeData.data[month].constantGrowthAssets[0].value == \
-            pytest.approx(
-                financeData.data[month-1].constantGrowthAssets[0].value * monthlyInterest)
+        event = financeData.data[month].events['g']
+        assert financeData.data[month].events['g'].value == \
+            pytest.approx(financeData.data[month-1].events['g'].value * monthlyInterest)
 
 def testConstantGrowthAnnually():
-    initialState = FinanceState(date(2001, 1, 1))
-    initialState.constantGrowthAssets.append(
-        ConstantGrowthAsset(AccrualModel.PeriodicYearly, 100, 0.5))
-    financeData = FinanceHistory(initialState)
-    financeData.setEventComponents([appreciateConstantAssets])
+    eventGroup = EventGroup(date(2001, 1, 1),
+                            { 'g': ConstantGrowthAsset('g',
+                                                       AccrualModel.PeriodicYearly,
+                                                       100,
+                                                       0.5),
+                              'cash': CashEvent('cash', 0) })
+    financeData = FinanceHistory(eventGroup)
     delta = relativedelta(years=1)
     for year, idx in zip(range(2002, 2010), range(1, 9)):
         financeData.passEvent(date(year, 1, 1), delta)
-        assert financeData.data[idx].constantGrowthAssets[0].value == \
-            pytest.approx(financeData.data[idx-1].constantGrowthAssets[0].value * 1.5)
+        assert financeData.data[idx].events['g'].value == \
+            pytest.approx(financeData.data[idx-1].events['g'].value * 1.5)
 
