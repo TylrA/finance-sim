8d60c555da84c660a79e447ace7377d6e25fcd4d
tyler <tylerhaddams@gmail.com>
2023-12-06 23:47:07 -0700

    fix amortizing loan tests

--- test/amortizing-loan.py
+++ test/amortizing-loan.py
@@ -5,52 +5,64 @@ from dateutil.relativedelta import relativedelta
 
 def testAmortizingLoanTerm():
     loan = AmortizingLoan('test', AccrualModel.PeriodicMonthly, 0, 100000, 0.05, 20)
-    state = FinanceState(date(1999, 12, 1))
-    state.cash = 100000
-    history = FinanceHistory(state)
-    state.amortizingLoans['test'] = loan
+    eventGroup = EventGroup(date(1999, 12, 1), {'test': loan,
+                                                'cash': CashEvent('cash', 600000)})
+    history = FinanceHistory(eventGroup)
     delta = relativedelta(months=1)
-    state = makeAmortizedPayments(history, state, date(2000, 1, 1), delta)
-    assert state.amortizingLoans['test'].term == pytest.approx(20 - 1 / 12)
+    history.passEvent(date(2000, 1, 1), delta)
+    event = history.latestEvents().events['test']
+    assert isinstance(event, AmortizingLoan)
+    assert event.term == pytest.approx(20 - 1 / 12)
     for month in range(2, 13):
-        state = makeAmortizedPayments(history, state, date(2000, month, 1), delta)
-    assert state.amortizingLoans['test'].term == pytest.approx(19)
+        history.passEvent(date(2000, month, 1), delta)
+    event = history.latestEvents().events['test']
+    assert isinstance(event, AmortizingLoan)
+    assert event.term == pytest.approx(19)
 
 def testAmortizingLoanFullPayment():
     loan = AmortizingLoan('test', AccrualModel.PeriodicMonthly, 0, 100000, 0.05, 20)
-    state = FinanceState(date(1999, 12, 1))
-    state.cash = 600000
-    history = FinanceHistory(state)
-    state.amortizingLoans['test'] = loan
-    assert state.amortizingLoans['test'].principle == 0
+    eventGroup = EventGroup(date(1999, 12, 1), { 'test': loan,
+                                                 'cash': CashEvent('cash', 600000) })
+    history = FinanceHistory(eventGroup)
+    event = history.latestEvents().events['test']
+    assert isinstance(event, AmortizingLoan)
+    assert event.principle == 0
     delta = relativedelta(months=1)
     for year in range(2000,2020):
         for month in range(1, 13):
-            state = makeAmortizedPayments(history, state, date(year, month, 1), delta)
-    assert state.amortizingLoans['test'].principle == pytest.approx(100000)
+            history.passEvent(date(year, month, 1), delta)
+    event = history.latestEvents().events['test']
+    assert isinstance(event, AmortizingLoan)
+    assert event.principle == pytest.approx(100000)
 
 def testAmortizingLoanConstantPaymentCost():
     loan = AmortizingLoan('test', AccrualModel.PeriodicMonthly, 0, 100000, 0.05, 20)
-    state = FinanceState(date(1999, 12, 1))
-    state.cash = 600000
-    history = FinanceHistory(state)
-    state.amortizingLoans['test'] = loan
+    eventGroup = EventGroup(date(1999, 12, 1), { 'test': loan,
+                                                 'cash': CashEvent('cash', 600000) })
+    history = FinanceHistory(eventGroup)
     delta = relativedelta(months=1)
-    state = makeAmortizedPayments(history, state, date(2000, 1, 1), delta)
-    previousCash = state.cash
-    paymentAmount = 600000 - previousCash
+    history.passEvent(date(2000, 1, 1), delta)
+    previousCash = history.latestEvents().events['cash']
+    assert isinstance(previousCash, CashEvent)
+    paymentAmount = 600000 - previousCash.value
     for month in range(2, 13):
-        state = makeAmortizedPayments(history, state, date(2000, month, 1), delta)
-        assert previousCash - state.cash == pytest.approx(paymentAmount, 1e-3)
-        previousCash = state.cash
+        history.passEvent(date(2000, month, 1), delta)
+        newCash = history.latestEvents().events['cash']
+        assert isinstance(newCash, CashEvent)
+        assert previousCash.value - newCash.value == pytest.approx(paymentAmount, 1e-3)
+        previousCash = newCash
 
 def testAmortizingLoanInterestRate():
-    loan = AmortizingLoan('test', AccrualModel.PeriodicYearly, 0, 100000, 0.05, 20)
-    state = FinanceState(date(2000, 1, 1))
-    state.cash = 600000
-    history = FinanceHistory(state)
-    state.amortizingLoans['test'] = loan
-    state = makeAmortizedPayments(history, state, date(2001, 1, 1), relativedelta(years=1))
-    paymentAmount = 600000 - state.cash
-    interestPayment = paymentAmount - state.amortizingLoans['test'].principle
+    loan = AmortizingLoan('test', AccrualModel.PeriodicMonthly, 0, 100000, 0.05, 20)
+    eventGroup = EventGroup(date(2000, 1, 1), { 'test': loan,
+                                                 'cash': CashEvent('cash', 600000) })
+    history = FinanceHistory(eventGroup)
+    delta = relativedelta(years=1)
+    history.passEvent(date(2001, 1, 1), delta)
+    cashEvent = history.latestEvents().events['cash']
+    assert isinstance(cashEvent, CashEvent)
+    paymentAmount = 600000 - cashEvent.value
+    loanEvent = history.latestEvents().events['test']
+    assert isinstance(loanEvent, AmortizingLoan)
+    interestPayment = paymentAmount - loanEvent.principle
     assert interestPayment == pytest.approx(100000 * 0.05, 1e-6)
