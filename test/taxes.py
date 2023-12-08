264dd017d34d198df068bb4386c0261e4681524e
tyler <tylerhaddams@gmail.com>
2023-12-07 20:36:46 -0700

    fix taxes tests

--- test/taxes.py
+++ test/taxes.py
@@ -5,65 +5,62 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 def testSalariedIncomeFlatTax():
-    history = FinanceHistory(FinanceState(date(1999, 12, 1)))
     delta = relativedelta(months=1)
-    history.setEventComponents(
-        [constantSalariedIncome(100000, AccrualModel.PeriodicMonthly),
-         taxPaymentSchedule(delta,
-                            AccrualModel.PeriodicMonthly,
-                            [TaxBracket(rate = 0.05, income = 0)])])
+    events = {
+        'i': ConstantSalariedIncome('i', 100000, AccrualModel.PeriodicMonthly),
+        't': TaxPaymentEvent('t',
+                             delta,
+                             AccrualModel.PeriodicMonthly,
+                             [TaxBracket(rate = 0.05, income = 0)]),
+        'c': CashEvent('c', 0) }
+    history = FinanceHistory(EventGroup(date(1999, 12, 1), events))
     for month in range(1, 13):
         history.passEvent(date(2000, month, 1), delta)
-    assert history.latestState().cash == pytest.approx(100000 * (1 - 0.05))
-    assert history.latestState().taxableIncome == pytest.approx(0)
-    assert history.latestState().taxesPaid == pytest.approx(5000)
+    assert history.latestEvents().events['c'].value == pytest.approx(100000 * (1 - 0.05))
+    assert history.latestEvents().events['t'].taxableIncome == pytest.approx(0)
+    assert history.latestEvents().events['t'].taxesPaid == pytest.approx(5000)
 
 def testIsolatedTaxableIncome():
-    history = FinanceHistory(FinanceState(date(1999, 1, 1)))
     delta = relativedelta(years=1)
-    history.setEventComponents(
-        [taxPaymentSchedule(delta,
-                            AccrualModel.PeriodicYearly,
-                            [TaxBracket(rate = 0.05, income = 0)])])
-    history.latestState().taxableIncome = 100000
+    events = { 't': TaxPaymentEvent('t',
+                                    delta,
+                                    AccrualModel.PeriodicYearly,
+                                    [TaxBracket(rate = 0.05, income = 0)]),
+               'c': CashEvent('c', 100000) }
+    history = FinanceHistory(EventGroup(date(1999, 1, 1), events))
+    history.latestEvents().events['t'].taxableIncome = 100000
     history.passEvent(date(2000, 1, 1), delta)
-    assert history.latestState().cash == pytest.approx(100000 * (- 0.05))
-    assert history.latestState().taxableIncome == pytest.approx(0)
-    assert history.latestState().taxesPaid == pytest.approx(5000)
+    assert history.latestEvents().events['c'].value == pytest.approx(100000 * (1 - 0.05))
+    assert history.latestEvents().events['t'].taxableIncome == pytest.approx(0)
+    assert history.latestEvents().events['t'].taxesPaid == pytest.approx(5000)
 
 def testMultipleTaxBrackets():
-    history = FinanceHistory(FinanceState(date(1999, 1, 1)))
     delta = relativedelta(years=1)
-    history.setEventComponents(
-        [taxPaymentSchedule(delta,
-                            AccrualModel.PeriodicYearly,
-                            [TaxBracket(rate = 0.05, income = 0),
-                             TaxBracket(rate = 0.10, income = 40000),
-                             TaxBracket(rate = 0.20, income = 60000)])])
-    history.latestState().taxableIncome = 100000
+    events = { 't': TaxPaymentEvent('t',
+                                    delta,
+                                    AccrualModel.PeriodicYearly,
+                                    [TaxBracket(0.05, 0),
+                                     TaxBracket(0.10, 40000),
+                                     TaxBracket(0.20, 60000)]),
+               'c': CashEvent('c', 100000) }
+    history = FinanceHistory(EventGroup(date(1999, 1, 1), events))
+    history.latestEvents().events['t'].taxableIncome = 100000
     history.passEvent(date(2000, 1, 1), delta)
     expectedTaxPaid = 40000 * 0.05 + 20000 * 0.10 + 40000 * 0.20
-    assert history.latestState().cash == pytest.approx(-expectedTaxPaid)
-    assert history.latestState().taxableIncome == pytest.approx(0)
-    assert history.latestState().taxesPaid == pytest.approx(expectedTaxPaid)
+    assert history.latestEvents().events['c'].value == pytest.approx(100000-expectedTaxPaid)
+    assert history.latestEvents().events['t'].taxableIncome == pytest.approx(0)
+    assert history.latestEvents().events['t'].taxesPaid == pytest.approx(expectedTaxPaid)
 
 def testMissingTaxBrackets():
     with pytest.raises(ArgumentError):
-        history = FinanceHistory(FinanceState(date(1999, 12, 1)))
-        delta = relativedelta(months=1)
-        history.setEventComponents([taxPaymentSchedule(delta,
-                                                       AccrualModel.PeriodicMonthly,
-                                                       [])])
-        history.latestState().taxableIncome = 100000
-        history.passEvent(date(2000, 1, 1), delta)
+        TaxPaymentEvent('t',
+                        relativedelta(months=1),
+                        AccrualModel.PeriodicMonthly,
+                        [])
 
 def testNonZeroTaxBracket():
     with pytest.raises(ArgumentError):
-        history = FinanceHistory(FinanceState(date(1999, 12, 1)))
-        delta = relativedelta(months=1)
-        history.setEventComponents(
-            [taxPaymentSchedule(delta,
-                                AccrualModel.PeriodicMonthly,
-                                [TaxBracket(rate = 0.05, income = 10)])])
-        history.latestState().taxableIncome = 100000
-        history.passEvent(date(2000, 1, 1), delta)
+        TaxPaymentEvent('t',
+                        relativedelta(months=1),
+                        AccrualModel.PeriodicMonthly,
+                        [TaxBracket(rate = 0.05, income = 10)])
