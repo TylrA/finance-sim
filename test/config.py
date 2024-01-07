0ce14e048fe7e80dae0e1fe10ef714a98267a7ed
tyler <tylerhaddams@gmail.com>
2024-01-06 23:22:52 -0700

    update config tests

--- test/config.py
+++ test/config.py
@@ -15,10 +15,10 @@ def testParseConfigInitialState():
     path = 'examples/finance-config.yaml'
     config = parseConfig(path)
     assert len(config.initialState) == 2
-    assert config.initialState[0].type == StateType.cash
+    assert config.initialState[0].type == 'cash'
     assert config.initialState[0].name == "Savings"
     assert config.initialState[0].data == {"value": 100}
-    assert config.initialState[1].type == StateType.constantGrowthAsset
+    assert config.initialState[1].type == 'constant-growth-asset'
     assert config.initialState[1].name == "CD Savings"
     assert config.initialState[1].data == {"value": 500, "appreciation": 0.05}
     # todo: add appreciation
@@ -30,5 +30,5 @@ def testParseConfigScheduledUpdates():
     assert config.scheduledValues[0].startDate == date(2030, 5, 15)
     assert config.scheduledValues[0].endDate == date(2040, 4, 30)
     assert config.scheduledValues[0].state.name == "inheritance"
-    assert config.scheduledValues[0].state.type == StateType.cash
+    assert config.scheduledValues[0].state.type == 'cash'
     assert config.scheduledValues[0].state.data == {"value": 5000}
