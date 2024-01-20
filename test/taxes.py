e4a5fbc61b46bfad98a6b9fbe560b734ffe60aea
tyler <tylerhaddams@gmail.com>
2024-01-19 23:16:05 -0700

    fix tax tests

--- test/taxes.py
+++ test/taxes.py
@@ -8,13 +8,13 @@ def testSalariedIncomeFlatTax():
     delta = relativedelta(months=1)
     events = {
         'i': ConstantSalariedIncome(None, 'i', 100000, AccrualModel.PeriodicMonthly),
-        't': TaxPaymentEvent(None,
+        't': TaxPaymentEventProfile(None,
                              't',
                              delta,
                              AccrualModel.PeriodicMonthly,
                              [TaxBracket(rate = 0.05, income = 0)]),
-        'c': CashEvent(None, 'c', 0) }
-    history = FinanceHistory(EventGroup(date(1999, 12, 1), events))
+        'c': CashEventProfile(None, 'c', 0) }
+    history = FinanceHistory(EventProfileGroup(date(1999, 12, 1), events))
     for month in range(1, 13):
         history.passEvent(date(2000, month, 1), delta)
     assert history.latestEvents().events['c'].value == pytest.approx(100000 * (1-0.05))
@@ -23,13 +23,13 @@ def testSalariedIncomeFlatTax():
 
 def testIsolatedTaxableIncome():
     delta = relativedelta(years=1)
-    events = { 't': TaxPaymentEvent(None,
+    events = { 't': TaxPaymentEventProfile(None,
                                     't',
                                     delta,
                                     AccrualModel.PeriodicYearly,
                                     [TaxBracket(rate = 0.05, income = 0)]),
-               'c': CashEvent(None, 'c', 100000) }
-    history = FinanceHistory(EventGroup(date(1999, 1, 1), events))
+               'c': CashEventProfile(None, 'c', 100000) }
+    history = FinanceHistory(EventProfileGroup(date(1999, 1, 1), events))
     history.latestEvents().events['t'].taxableIncome = 100000
     history.passEvent(date(2000, 1, 1), delta)
     assert history.latestEvents().events['c'].value == pytest.approx(100000 * (1 - 0.05))
@@ -38,15 +38,15 @@ def testIsolatedTaxableIncome():
 
 def testMultipleTaxBrackets():
     delta = relativedelta(years=1)
-    events = { 't': TaxPaymentEvent(None,
+    events = { 't': TaxPaymentEventProfile(None,
                                     't',
                                     delta,
                                     AccrualModel.PeriodicYearly,
                                     [TaxBracket(0.05, 0),
                                      TaxBracket(0.10, 40000),
                                      TaxBracket(0.20, 60000)]),
-               'c': CashEvent(None, 'c', 100000) }
-    history = FinanceHistory(EventGroup(date(1999, 1, 1), events))
+               'c': CashEventProfile(None, 'c', 100000) }
+    history = FinanceHistory(EventProfileGroup(date(1999, 1, 1), events))
     history.latestEvents().events['t'].taxableIncome = 100000
     history.passEvent(date(2000, 1, 1), delta)
     expectedTaxPaid = 40000 * 0.05 + 20000 * 0.10 + 40000 * 0.20
@@ -56,7 +56,7 @@ def testMultipleTaxBrackets():
 
 def testMissingTaxBrackets():
     with pytest.raises(ArgumentError):
-        TaxPaymentEvent(None,
+        TaxPaymentEventProfile(None,
                         't',
                         relativedelta(months=1),
                         AccrualModel.PeriodicMonthly,
@@ -64,7 +64,7 @@ def testMissingTaxBrackets():
 
 def testNonZeroTaxBracket():
     with pytest.raises(ArgumentError):
-        TaxPaymentEvent(None,
+        TaxPaymentEventProfile(None,
                         't',
                         relativedelta(months=1),
                         AccrualModel.PeriodicMonthly,
