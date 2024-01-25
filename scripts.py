c9e0eb3297769d8fbff6bb373ba082bc46588150
tyler <tylerhaddams@gmail.com>
2024-01-24 23:16:33 -0700

    add script to format

--- scripts.py
+++ scripts.py
@@ -1,5 +1,11 @@
 import subprocess
 
+
 def runTests():
     command = "pytest test/*.py"
+    subprocess.run(command, shell=True, check=True)
+
+
+def format():
+    command = "black ."
     subprocess.run(command, shell=True, check=True)
